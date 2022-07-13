478c289662088632f988c65bc09d79d89a8fe487
Sergio Viana <svfreitasd@yahoo.com>
2022-07-12 21:10:35 -0300

    first version

---
+++ config.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net"
+	"strconv"
+
+	"github.com/j-keck/arping"
+)
+
+type rawConfig struct {
+	InterfaceName      string   `json:"interface_name"`
+	IpAddressReceiver  string   `json:"ip_address_receiver"`
+	MacAddressReceiver string   `json:"mac_address_receiver"`
+	DefaultGateway     string   `json:"default_gateway"`
+	MaxPacketSize      int      `json:"max_packet_size"`
+	Destinations       []string `json:"destinations"`
+}
+
+type Config struct {
+	InterfaceName           string
+	IpAddressReceiver       net.IP
+	UdpPortReceiver         uint16
+	MacAddresDefaultGateway net.HardwareAddr
+	MacAddressReceiver      net.HardwareAddr
+	MaxPacketSize           int
+	Destinations            []Destination
+}
+
+type Destination struct {
+	IpAddress  net.IP
+	MacAddress net.HardwareAddr
+	Port       uint16
+}
+
+func LoadConfiguration() (*Config, error) {
+
+	var config Config
+	var rawConfig rawConfig
+	var err error
+
+	content, err := ioutil.ReadFile("./udp-fwd.json")
+
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal([]byte(content), &rawConfig)
+
+	if err != nil {
+		log.Fatalf("Configuration file problem : %s", err)
+	}
+
+	config.InterfaceName = rawConfig.InterfaceName
+
+	host, port, err := net.SplitHostPort(rawConfig.IpAddressReceiver)
+	if err != nil {
+		log.Fatal("Host Port bad format")
+	}
+
+	config.IpAddressReceiver = net.ParseIP(host)
+	if config.IpAddressReceiver == nil {
+		log.Fatal("IpAddressReceiver bad format")
+	}
+	log.Print(config.IpAddressReceiver.String())
+	if hwAddr, _, err := arping.Ping(config.IpAddressReceiver); err != nil {
+		log.Printf("MAC address resolution problem for Receiver: %s, will use the provided one in the configuration", err)
+		config.MacAddressReceiver, err = net.ParseMAC(rawConfig.MacAddressReceiver)
+		if err != nil {
+			log.Fatal("MacAddressReceiver bad format")
+		}
+	} else {
+		log.Printf("%s is at %s\n", config.IpAddressReceiver.String(), hwAddr)
+		config.MacAddresDefaultGateway = hwAddr
+	}
+
+	portUint, err := strconv.ParseUint(port, 10, 16)
+	if err != nil {
+		log.Fatal("Port bad format")
+	}
+	config.UdpPortReceiver = uint16(portUint)
+
+	config.MaxPacketSize = rawConfig.MaxPacketSize
+
+	dg := net.ParseIP(rawConfig.DefaultGateway)
+	if dg == nil {
+		log.Fatal("DefaultGateway bad format")
+	}
+
+	if hwAddr, _, err := arping.Ping(dg); err != nil {
+		log.Fatalf("MAC address resolution problem for DefaultGateway: %s", err)
+	} else {
+		log.Printf("%s is at %s\n", dg.String(), hwAddr)
+		config.MacAddresDefaultGateway = hwAddr
+	}
+
+	for _, rawDest := range rawConfig.Destinations {
+		var dest Destination
+		host, port, err := net.SplitHostPort(rawDest)
+		if err != nil {
+			log.Fatalf("Host Port bad format for Destination: %s", rawDest)
+		}
+
+		dest.IpAddress = net.ParseIP(host)
+		if dest.IpAddress == nil {
+			log.Fatalf("IpAddressDestination bad format for Destination:%s", rawDest)
+		}
+
+		uPort, err := strconv.ParseUint(port, 10, 16)
+		if err != nil {
+			log.Fatalf("Port bad format for Destination: %s", rawDest)
+		}
+		dest.Port = uint16(uPort)
+
+		var hwAddr net.HardwareAddr
+		if hwAddr, _, err = arping.Ping(dest.IpAddress); err != nil {
+			log.Printf("MAC address resolution problem for Destination: %s, setting Default Gateway MAC Address", dest.IpAddress.String())
+			hwAddr = config.MacAddresDefaultGateway
+		}
+		dest.MacAddress = hwAddr
+
+		config.Destinations = append(config.Destinations, dest)
+	}
+
+	return &config, nil
+
+}
---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+)
+
+func main() {
+	config, err := LoadConfiguration()
+
+	if err != nil {
+		log.Printf("Unable to load configuration, error :%s", err)
+		return
+	}
+
+	handle, err := pcap.OpenLive(config.InterfaceName, int32(config.MaxPacketSize), false, pcap.BlockForever)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+
+	bpfFilter := fmt.Sprintf("udp and port %d", config.UdpPortReceiver)
+	if err := handle.SetBPFFilter(bpfFilter); err != nil {
+		panic(err)
+	}
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+
+	for packet := range packetSource.Packets() {
+		//start := time.Now()
+		handlePacket(handle, packet, config)
+		//elapsed := time.Since(start)
+		//log.Printf("trap redirection takes %s", elapsed)
+	}
+
+}
+
+func handlePacket(handle *pcap.Handle, packet gopacket.Packet, config *Config) {
+	var udpFrameOptions UdpFrameOptions
+
+	udpFrameOptions.sourceMac = config.MacAddressReceiver
+	log.Print("-------------------------------------")
+	log.Printf("udpFrameOptions.sourceMac = %v", udpFrameOptions.sourceMac)
+
+	if netw := packet.NetworkLayer(); netw != nil {
+		srcN, _ := netw.NetworkFlow().Endpoints()
+		if transp := packet.TransportLayer(); transp != nil {
+			if app := packet.ApplicationLayer(); app != nil {
+				data := app.Payload()
+				udpFrameOptions.sourceIP = net.ParseIP(srcN.String())
+				udpFrameOptions.payloadBytes = data
+				log.Printf("udpFrameOptions.sourceIP = %v", udpFrameOptions.sourceIP)
+			}
+		}
+	}
+
+	for _, destination := range config.Destinations {
+
+		udpFrameOptions.destIP = destination.IpAddress
+		udpFrameOptions.destMac = destination.MacAddress
+		udpFrameOptions.destPort = destination.Port
+		udpFrameOptions.isIPv6 = false
+
+		log.Printf("udpFrameOptions.destIP = %v", udpFrameOptions.destIP)
+		log.Printf("udpFrameOptions.destPort = %v", udpFrameOptions.destPort)
+		log.Printf("udpFrameOptions.destMac = %v", udpFrameOptions.destMac)
+
+		frameBytes, err := createSerializedUDPFrame(udpFrameOptions)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err := handle.WritePacketData(frameBytes); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
